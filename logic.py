bf63f47644ff408bbcc8ca886dad28d9f9c51325
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-04-09 23:08:56 +0200

    Fixed Import loop and started threading plan

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 from libraries import *
-from main import window
 
-class logic():
+class time_logic():
 
     def shutdown() -> None:
         log.info("Shutting down the system...")
@@ -12,29 +11,35 @@ class logic():
             current_time = t.localtime()
             log.info(f"Current time: {current_time.tm_year}, {current_time.tm_mday} ,{current_time.tm_hour}:{current_time.tm_min}:{current_time.tm_sec}")
             if current_time.tm_hour in range(1,6) or current_time.tm_hour in [23,24]:
-                logic.is_the_time_right(current_time)
-                window.main_window()
+                #There will be one thread
+                time_logic.is_the_time_right(current_time)
+                ### From Here to
+                msbox = True
                 t.sleep(3600)
+                return current_time, msbox
+                ### Here will be second thread to start timer and show msbox at once
             t.sleep(1)
-            return current_time
+            msbox = False
     
     def is_the_time_right(current_time) -> None:
         note_down_time = current_time
         log.info(f"Note down time: {note_down_time.tm_year}, {note_down_time.tm_mday} ,{note_down_time.tm_hour}:{note_down_time.tm_min}:{note_down_time.tm_sec}")
 
         # Give user ten minutes to take action
-        if note_down_time.th_min > 50:
+        if note_down_time.tm_min > 50:
+            log.info("Countdown is in progress")
             while True:
                 if note_down_time.tm_min + 10 in [0,60]:
                     log.info("Shut down is in progress")
-                    logic.shutdown()
+                    time_logic.shutdown()
                     break
                 t.sleep(1)
 
         else:
+            log.info("Countdown is in progress")
             while True:
                 if note_down_time.tm_min + 10 == t.localtime().tm_min:
                     log.info("Shut down is in progress")
-                    logic.shutdown()
+                    time_logic.shutdown()
                     break
                 t.sleep(1)
--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from libraries import *
-from logic import logic, what_is_the_time
+from logic import time_logic
 
 log.basicConfig(level=log.INFO, filename="app_log")
 
 class window():
 
-    def main_window():
+    def main_window() -> None:
         root = tk.Tk()
         root.title("Main Window")
         root.geometry("300x200")
-        root.withdraw()
+        #root.withdraw()
 
-        logic.what_is_the_time()
+        time_logic.what_is_the_time(root)
+        current_time, msbox= time_logic.what_is_the_time(root)
+        if msbox:
+            messagebox.askokcancel("Quit", "Do you want to leave?")
 
         root.mainloop()
 
