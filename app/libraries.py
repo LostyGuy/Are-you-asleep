4b70735cc9b1e7b7e95133f3ec4d793db6b508e7
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2025-08-01 20:18:52 +0200

    Trying to deploy app via pyinstaller

--- app/libraries.py
+++ app/libraries.py
@@ -7,5 +7,8 @@ from multiprocessing import Process
 import logging as log
 import shutil
 
+# Creating Virtual Environment: python -m venv .venv
+# Activating Virtual Environment: .venv\Scripts\activate
+
 # Get requirements: pip freeze > requirements.txt
 # Installing Dependencies: pip install -r requirements.txt
